c4bb1c1b6431f3ddb6379f952f0e8845f57abc0f
RedkarRiv <128104355+RedkarRiv@users.noreply.github.com>
2023-04-29 21:17:51 +0200

    Botones para cambio de color y funcionalidad correctamente aplicada

---
+++ main.js
@@ -0,0 +1,71 @@
+let color1 = "#F9CB49" || "#F9CB49";
+let color2 = "#d6ae3f" || "#d6ae3f";
+let color3 = "#e1ad1c" || "#e1ad1c";
+let color4 = "#7a5c07" || "#7a5c07";
+let color5 = "orange" || "orange";
+
+document.documentElement.style.setProperty('--color1', color1);
+document.documentElement.style.setProperty('--color2', color2);
+document.documentElement.style.setProperty('--color3', color3);
+document.documentElement.style.setProperty('--color4', color4);
+document.documentElement.style.setProperty('--color5', color5);
+
+
+
+document.querySelector('.botonazul').addEventListener('click', function () {
+    color1 = "#495df9";
+    color2 = "#3f4ed6";
+    color3 = "#1c1fe1";
+    color4 = "#0d077a";
+    color5 = "blue";
+
+    document.documentElement.style.setProperty('--color1', color1);
+    document.documentElement.style.setProperty('--color2', color2);
+    document.documentElement.style.setProperty('--color3', color3);
+    document.documentElement.style.setProperty('--color4', color4);
+    document.documentElement.style.setProperty('--color5', color5);
+});
+
+document.querySelector('.botonamarillo').addEventListener('click', function () {
+    color1 = "#F9CB49";
+    color2 = "#d6ae3f";
+    color3 = "#e1ad1c";
+    color4 = "#7a5c07";
+    color5 = "orange";
+
+    document.documentElement.style.setProperty('--color1', color1);
+    document.documentElement.style.setProperty('--color2', color2);
+    document.documentElement.style.setProperty('--color3', color3);
+    document.documentElement.style.setProperty('--color4', color4);
+    document.documentElement.style.setProperty('--color5', color5);
+});
+
+document.querySelector('.botonrojo').addEventListener('click', function () {
+    color1 = "#fb3434";
+    color2 = "#d63f3f";
+    color3 = "#e11c1c";
+    color4 = "#7a0707";
+    color5 = "red";
+
+    document.documentElement.style.setProperty('--color1', color1);
+    document.documentElement.style.setProperty('--color2', color2);
+    document.documentElement.style.setProperty('--color3', color3);
+    document.documentElement.style.setProperty('--color4', color4);
+    document.documentElement.style.setProperty('--color5', color5);
+});
+
+document.querySelector('.botonverde').addEventListener('click', function () {
+    color1 = "#83e741";
+    color2 = "#41d63f";
+    color3 = "#1ce133";
+    color4 = "#077a11";
+    color5 = "green";
+
+    document.documentElement.style.setProperty('--color1', color1);
+    document.documentElement.style.setProperty('--color2', color2);
+    document.documentElement.style.setProperty('--color3', color3);
+    document.documentElement.style.setProperty('--color4', color4);
+    document.documentElement.style.setProperty('--color5', color5);
+});
+
+
